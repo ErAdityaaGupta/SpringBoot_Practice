19f5ac991b0d3ed547c2eb16f6c513745e6d3956
Aditya Gupta <ingenious.adig@gmail.com>
2024-05-15 16:48:42 +0530

    Initial commit

--- mycoolapp/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -6,9 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
+    // expose "/" to return "Hello world"
     @GetMapping("/")
     public String sayHello(){
         return "Hello world";
+    }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a mile or so";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day!";
     }
 
 }
